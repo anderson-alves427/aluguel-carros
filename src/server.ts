e5afe6895d554f0beb441db586735a57ac0b73bd
anderson-alves427 <oliveiraalves427@gmail.com>
2022-10-16 21:34:32 -0300

    Configurando debuger

--- src/server.ts
+++ src/server.ts
@@ -2,4 +2,14 @@ import express from "express";
 
 const app = express();
 
+app.use(express.json());
+
+app.get("/teste_debug", (request, response) => {
+    const { nome } = request.query;
+
+    console.log("---", nome);
+
+    return response.send("ok");
+});
+
 app.listen(8080);
