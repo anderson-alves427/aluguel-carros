ba2e41de36916299a007c9ffb3e82882bee19e72
anderson-alves427 <oliveiraalves427@gmail.com>
2022-10-23 15:30:33 -0300

    Ajustando rotas

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import express from "express";
 
+import { router } from "./routes";
 import { categoriesRoutes } from "./routes/categories.routes";
 import { specificationRoutes } from "./routes/specification.routes";
 
@@ -7,8 +8,6 @@ const app = express();
 
 app.use(express.json());
 
-app.use("/categories", categoriesRoutes);
-
-app.use("/specification", specificationRoutes);
+app.use(router);
 
 app.listen(8080);
